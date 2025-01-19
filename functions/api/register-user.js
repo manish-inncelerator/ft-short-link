b9e7ecc9344928cd501cadb8fa8bd2b7992551cb
manish.shahi <manish.shahi@infraveo.com>
2025-01-19 10:24:26 +0530

    fixed await to context

--- functions/api/register-user.js
+++ functions/api/register-user.js
@@ -4,7 +4,7 @@ export async function onRequestPost(context) {
   const username = formdata.get("user-name-1");
   const email = formdata.get("email-2");
 
-  context.env.USER_DATA_STORE.put(username, email);
+  await context.env.USER_DATA_STORE.put(username, email);
 
   return new Response(`${username} - ${email}`);
 }
