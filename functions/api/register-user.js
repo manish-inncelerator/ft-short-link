36bbf6a2baadab9f3beda9a2f07c11e5f407f2f5
manish.shahi <manish.shahi@infraveo.com>
2025-01-19 10:16:25 +0530

    Added await

--- functions/api/register-user.js
+++ functions/api/register-user.js
@@ -1,6 +1,6 @@
 export async function onRequestPost(context) {
   // grab from data
-  const formdata = context.request.formData();
+  const formdata = await context.request.formData();
   const username = formdata.get("user-name-1");
   const email = formdata.get("email-2");
   return new Response(`${username} - ${email}`);
