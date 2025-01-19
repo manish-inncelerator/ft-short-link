db50ff7781b7897a3129bf408459341ceb378787
manish.shahi <manish.shahi@infraveo.com>
2025-01-19 10:23:20 +0530

    added KV store

--- functions/api/register-user.js
+++ functions/api/register-user.js
@@ -3,5 +3,8 @@ export async function onRequestPost(context) {
   const formdata = await context.request.formData();
   const username = formdata.get("user-name-1");
   const email = formdata.get("email-2");
+
+  context.env.USER_DATA_STORE.put(username, email);
+
   return new Response(`${username} - ${email}`);
 }
